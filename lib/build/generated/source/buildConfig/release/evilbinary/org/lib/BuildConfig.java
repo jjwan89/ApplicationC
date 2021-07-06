1665c0595c60cd0acd759927fd2fb642a2ed55d6
jjwan89 <1650804501@qq.com>
2021-07-06 13:46:33 +0800

    初始版本

---
+++ lib/build/generated/source/buildConfig/release/evilbinary/org/lib/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package evilbinary.org.lib;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "evilbinary.org.lib";
+  public static final String BUILD_TYPE = "release";
+}
